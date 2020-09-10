0d256577cd3951cb9799ffbeac6dde864772514c
Danis Desipris <danis_desipris@hotmail.com>
2020-09-10 16:25:39 +0300

    sort summary table

--- covid-19-data/src/Components/Summary/Summary.js
+++ covid-19-data/src/Components/Summary/Summary.js
@@ -20,11 +20,36 @@ function timeSinceLastUpdate(dateFromState) {
   return time;
 }
 
+function compareValues(key, order = 'asc') {
+  console.log(key)
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      return 0;
+    }
+
+    const varA = (typeof a[key] === 'string')
+      ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string')
+      ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return (
+      (order === 'desc') ? (comparison * -1) : comparison
+    );
+  };
+}
+
 class Summary extends React.Component {
   constructor(props) {
     super(props);
     this.state = null;
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.sortTable = this.sortTable.bind(this);
   }
 
   async componentDidMount() {
@@ -36,7 +61,59 @@ class Summary extends React.Component {
       delete data.Countries[i]["Date"];
       delete data.Countries[i]["Premium"];
     }
-    this.setState({ data });
+    let asc = {
+      NewConfirmed: true,
+      TotalConfirmed: true,
+      NewDeaths: true,
+      TotalDeaths: true,
+      NewRecovered: true,
+      TotalRecovered: true,
+      Country: true,
+    }
+    this.setState({ data, asc });
+  }
+
+  sortTable(index) {
+    let data = this.state.data.Countries
+    let sortBy
+    switch (index) {
+      case 2:
+        sortBy = "NewConfirmed";
+        break;
+      case 3:
+        sortBy = "TotalConfirmed";
+        break;
+      case 4:
+        sortBy = "NewDeaths";
+        break;
+      case 5:
+        sortBy = "TotalDeaths";
+        break;
+      case 6:
+        sortBy = "NewRecovered";
+        break;
+      case 7:
+        sortBy = "TotalRecovered";
+        break;
+      default:
+        sortBy = "Country";
+        break;
+    }
+
+    let ascOrDesc = !this.state.asc[sortBy];
+    let howToSort = 'asc'
+    if (ascOrDesc === false){ howToSort = 'desc'}
+
+    data.sort(compareValues(sortBy, howToSort));
+    this.setState(({data}) => ({
+      data
+    }));
+    this.setState(prevState  => ({
+      asc:{
+        ...prevState.asc,
+        [sortBy]: ascOrDesc
+      }
+    }));
   }
 
   render() {
@@ -63,7 +140,7 @@ class Summary extends React.Component {
             render={() => (
               <div>
                 <GeneralGlobally Globals={state.data.Global} />
-                <CountriesSummary CountriesSummary={state.data.Countries} />
+                <CountriesSummary sortTable={this.sortTable} CountriesSummary={state.data.Countries} />
               </div>
             )}
           />
